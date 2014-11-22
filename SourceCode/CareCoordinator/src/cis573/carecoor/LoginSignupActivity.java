0db1a4c9bcfdb447207ce78cd785127f2ea38e0d
xuzonghan <xuzonghan@gmail.com>
2014-11-21 22:14:08 -0500

    fix log in and sign up bug

--- SourceCode/CareCoordinator/src/cis573/carecoor/LoginSignupActivity.java
+++ SourceCode/CareCoordinator/src/cis573/carecoor/LoginSignupActivity.java
@@ -45,28 +45,39 @@ public class LoginSignupActivity extends Activity {
 				usernametxt = username.getText().toString();
 				passwordtxt = password.getText().toString();
 
-				// Send data to Parse.com for verification
-				ParseUser.logInInBackground(usernametxt, passwordtxt,
-						new LogInCallback() {
-							public void done(ParseUser user, ParseException e) {
-								if (user != null) {
-									// If user exist and authenticated, send user to Welcome.class
-									Intent intent = new Intent(
-											LoginSignupActivity.this,
-											MainActivity.class);
-									startActivity(intent);
-									Toast.makeText(getApplicationContext(),
-											"Successfully Logged in",
-											Toast.LENGTH_LONG).show();
-									finish();
-								} else {
-									Toast.makeText(
-											getApplicationContext(),
-											"No such user exist, please signup",
-											Toast.LENGTH_LONG).show();
+				// Force user to fill up the form
+				if (usernametxt.equals("") || passwordtxt.equals("")) {
+					Toast.makeText(getApplicationContext(),
+							"Please complete the sign up form",
+							Toast.LENGTH_LONG).show();
+
+				} else {
+					// Send data to Parse.com for verification
+					ParseUser.logInInBackground(usernametxt, passwordtxt,
+							new LogInCallback() {
+								public void done(ParseUser user,
+										ParseException e) {
+									if (user != null) {
+										// If user exist and authenticated, send
+										// user to Welcome.class
+										Intent intent = new Intent(
+												LoginSignupActivity.this,
+												MainActivity.class);
+										startActivity(intent);
+										Toast.makeText(getApplicationContext(),
+												"Successfully Logged in",
+												Toast.LENGTH_LONG).show();
+										finish();
+									} else {
+										Toast.makeText(
+												getApplicationContext(),
+												"No such user exist, please signup",
+												Toast.LENGTH_LONG).show();
+									}
 								}
-							}
-						});
+							});
+				}
+
 			}
 		});
 		// Sign up Button Click Listener
@@ -76,9 +87,9 @@ public class LoginSignupActivity extends Activity {
 				// Retrieve the text entered from the EditText
 				usernametxt = username.getText().toString();
 				passwordtxt = password.getText().toString();
-				
+
 				// Force user to fill up the form
-				if (usernametxt.equals("") && passwordtxt.equals("")) {
+				if (usernametxt.equals("") || passwordtxt.equals("")) {
 					Toast.makeText(getApplicationContext(),
 							"Please complete the sign up form",
 							Toast.LENGTH_LONG).show();
@@ -91,8 +102,10 @@ public class LoginSignupActivity extends Activity {
 					user.signUpInBackground(new SignUpCallback() {
 						public void done(ParseException e) {
 							if (e == null) {
-								// Show a simple Toast message upon successful registration
-								Toast.makeText(getApplicationContext(),
+								// Show a simple Toast message upon successful
+								// registration
+								Toast.makeText(
+										getApplicationContext(),
 										"Successfully Signed up, please log in.",
 										Toast.LENGTH_LONG).show();
 							} else {
